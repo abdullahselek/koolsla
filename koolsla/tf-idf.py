33c3c94ce5818dbead2479ace9a465c00ddee9e1
Abdullah Selek <abdullahselek@gmail.com>
2018-02-17 22:19:04 +0000

    A new function for finding similatiries.

--- koolsla/tf-idf.py
+++ koolsla/tf-idf.py
@@ -4,6 +4,7 @@
 import time
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 def train_engine(plots, min_df=3):
     # Initializing tf-idf vectorizer
@@ -16,3 +17,16 @@ def train_engine(plots, min_df=3):
     plots_tfidf = vectorizer.fit_transform(plots)
     # Pack and return the results
     return plots_tfidf
+
+def find_similarities(name, recommendations, plots_tfidf):
+    # Generate similarities
+    similarities = linear_kernel(plots_tfidf, plots_tfidf)
+    # Get similarity scores for the food
+    scores = list(enumerate(similarities[name]))
+    # Sort into descending order the scores
+    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    # Get the number of the recommendations asked
+    food_recommendations = sorted_scores[1:recommendations + 1]
+    # Get the indices of the recommendation food
+    food_indices = [i[1] for i in food_recommendations]
+    return food_indices
