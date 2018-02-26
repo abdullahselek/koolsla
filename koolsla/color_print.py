cf7292a5b0585993975791d2d92c3aad8962c143
Abdullah Selek <abdullahselek@gmail.com>
2018-02-26 20:38:58 +0000

    Create a new file to print out colored texts.

---
+++ koolsla/color_print.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+
+from colorama import init
+from termcolor import colored
+
+init()  # Termcolor support for win32
+
+# Colors for output texts
+red = 'red'
+green = 'green'
+yellow = 'yellow'
+blue = 'blue'
+
+def print_red(message):
+    """Print out red colored text.
+    Args:
+      message (str): Text.
+    """
+    print(colored(message, red))
+
+def print_green(message):
+    """Print out green colored text.
+    Args:
+      message (str): Text.
+    """
+    print(colored(message, green))
+
+def print_yellow(message):
+    """Print out yellow colored text.
+    Args:
+      message (str): Text.
+    """
+    print(colored(message, yellow))
+
+
+def print_blue(message):
+    """Print out blue colored text.
+    Args:
+      message (str): Text.
+    """
+    print(colored(message, blue))
