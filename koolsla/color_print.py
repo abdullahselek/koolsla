9691c1e73db284957a459f3f54346d00e7ea7a14
Abdullah Selek <abdullahselek@gmail.com>
2018-02-27 20:18:45 +0000

    Print dish with id and given color.

--- koolsla/color_print.py
+++ koolsla/color_print.py
@@ -18,6 +18,7 @@ def print_red(message):
     Args:
       message (str): Text.
     """
+
     print(colored(message, red))
 
 def print_green(message):
@@ -25,6 +26,7 @@ def print_green(message):
     Args:
       message (str): Text.
     """
+
     print(colored(message, green))
 
 def print_yellow(message):
@@ -32,6 +34,7 @@ def print_yellow(message):
     Args:
       message (str): Text.
     """
+
     print(colored(message, yellow))
 
 
@@ -40,4 +43,14 @@ def print_blue(message):
     Args:
       message (str): Text.
     """
+
     print(colored(message, blue))
+  
+def print_dish(name, dish_id, color):
+    """Print out dish name and id with given color.
+    Args:
+      name (str): Dish name.
+      dish_id (int): ID.
+    """
+
+    print(colored('* Name : ' + name + ' - ID : ' + str(dish_id), color))
